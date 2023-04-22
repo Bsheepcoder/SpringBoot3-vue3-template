9d22acad68ffd8879ac1e74009c0468ca45628e5
AD钙1 <10892745+ad-calcium-1@user.noreply.gitee.com>
2023-04-22 15:58:24 +0800

    idea测试提交

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         qxd(100);
+        MyCode(100,100);
     }
     public static void qxd(int n){
         System.out.println("我是开发者qxd，下面是我写的代码");
@@ -9,6 +10,15 @@ public class Main {
             sum = sum + i;
         }
         System.out.println(sum);
+        System.out.println("-----------------------------");
+    }
+    public static void MyCode(int m, int n){
+        System.out.println("我是开发者my，下面是我写的代码");
+        int count = 0;
+        for(int i = 0; i < m; i++){
+            count = count + n;
+        }
+        System.out.println(count);
         System.out.println("-----------------------------");
     }
 
