40df68dc1f1a7591cf0aae58cbfaadafeb360695
qxd <710418455@qq.com>
2023-04-22 12:24:31 +0800

    idea测试 写一个函数

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        qxd(100);
+    }
+    public static void qxd(int n){
+        System.out.println("我是开发者qxd，下面是我写的代码");
+        int sum = 0;
+        for(int i  = 0 ; i< n ; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+        System.out.println("-----------------------------");
+    }
+
+}
+
+
+
