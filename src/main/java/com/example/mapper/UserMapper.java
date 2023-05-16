98ddbc63bffaea1ab8d14203c6d1826d8bc31af2
qxd <710418455@qq.com>
2023-05-16 20:00:07 +0800

    readme修改

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -7,8 +7,6 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-
-
     @Select("select * from user where username = #{text} or mail = #{text}")
     Account findAccountByNameOrEmail(String text);
 }
