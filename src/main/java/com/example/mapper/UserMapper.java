d481ae076595f9e61bfdd9078a965077af876262
Bsheepcoder <18947336207@163.com>
2023-05-18 13:28:00 +0800

    邮箱校验完善

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -5,8 +5,14 @@ import com.example.entity.Account;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
     @Select("select * from user where username = #{text} or mail = #{text}")
     Account findAccountByNameOrEmail(String text);
+
+
+    @Select("select * from user where username = #{text} or mail = #{text}")
+    List<Account> outputuserlist(String text);
 }
--- src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
+++ src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
@@ -5,16 +5,37 @@ import com.example.entity.Account;
 import com.example.mapper.UserMapper;
 import com.example.service.AuthorizeService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class AuthorizeServiceImpl implements AuthorizeService {
+
+    @Value("${spring.mail.username}")
+    String from;
+
+
     //引入mapper
     @Resource
     UserMapper mapper;
+
+    @Resource
+    MailSender mailSender;
+
+
+    @Resource
+    StringRedisTemplate template;
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
@@ -29,6 +50,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
     }
 
 
+    //邮箱验证实现
     @Override
     public boolean sendValidateEmail(String email){
         /**
@@ -38,6 +60,24 @@ public class AuthorizeServiceImpl implements AuthorizeService {
          *  4.如果发送失败，把redis里面的刚插入的数据删除
          *  5.用户在注册时，再从Redis里面取出对应键值对，然后看验证码是否一致
          */
+
+        Random random  = new Random();
+        int code =random.nextInt(999999);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(email);
+        message.setSubject("您的验证邮件");
+        message.setText("验证码为：" + code);
+        try{
+            //发送邮件
+            mailSender.send(message);
+            template.opsForValue().set();
+            return true;
+        }  catch (MailException e){
+            e.printStackTrace();
+        }
         return false;
     }
+
+
 }
